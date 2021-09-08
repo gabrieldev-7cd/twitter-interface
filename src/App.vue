af875c3f4185a618334207d353d80e662fd5ec76
Gabriel Alexandre <gabriel.alexandre.dev@gmail.com>
2021-09-08 20:17:03 -0300

    Adicao do FontAwesome - Inclusão de cores no tailwind.config

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="flex container h-screen w-full">
+    <div class="w-1/5 border-r border-lighter border-indigo-800 px-8 py-2 ">
+      <button class="h-12 w-12 hover:bg-lightblue text-3xl rounded-full text-blue ">
+        <i class="fab fa-twitter"></i>
+      </button>
+      <div>
+        <button v-for="tab in tabs" :key="tab.id" class="hover:tex-blue flex items-center py-2 px-4 hover:bg-lightblue rounded-full mr-auto">
+          <i :class=" '${ tab.icon } text-2xl mr-4 ' "></i>
+          <p class="text-lg font-semibold text-left "> {{ tab.title }}</p>
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  data() {
+      return {
+        tabs:[
+          {icon: 'fas fa-home', title: 'Home', id:'home'},
+          {icon: 'fas fa-hashtag', title: 'Explore', id:'explore'},
+          {icon: 'fas fa-bell', title: 'Notifications', id:'notifications'},
+          {icon: 'fas fa-envelope', title: 'Messages', id:'messages'},
+          {icon: 'fas fa-bookmark', title: 'Bookmarks', id:'bookmarks'},
+          {icon: 'fas fa-clipboard-list', title: 'Lists', id:'lists'},
+        ]
+      }
   }
 }
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,28 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    container: {
+      center:true
+    },
+    extend: {
+      fontFamily: {
+        // sans:['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'blue': '#1DA1F2',
+        'darkblue': '#2795D9',
+        'lightblue': '#EFF9FF',
+        'dark': '#657786',
+        'light': '#AAB8C2',
+        'lighter': '#E1E8ED',
+        'lightest': '#F5F8FA',
+      }
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    // require('@tailwindcss/ui'),
+  ]
 }
